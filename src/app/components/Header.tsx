56fa599aa44ac71088363a2334e036c16d95f5ea
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-03 00:46:15 +0100

    Replace logo image with react-icons in Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,19 @@
-import Image from "next/image"
-import logo from "/public/padlock.png"
+import { FaLock } from "react-icons/fa6"
 import "../globals.css"
 import Link from "next/link"
 
 const Header = () => {
   return (
       <div className="header">
-          <div style={{display: "flex", alignItems:"center", flexDirection: "row", gap: "10px"}}>
-              <Image alt="brand logo" src={logo} width={30} className="image"></Image>
-              <span style={{color: 'white', fontWeight: "600"}}>Fpe Recover</span>
-          </div>
-          <nav>
+        <div style={{ display: "flex", alignItems: "center", flexDirection: "row", gap: "10px" }}>
+          <FaLock/>
+          <span style={{color: 'white', fontWeight: "600"}}>Fpe Recover</span>
+        </div>
+        <nav>
           <Link href="/">Home</Link>
           <Link href="/report_item">Report Lost Item</Link>
           <Link href="/">Contact</Link>
-          </nav>
+        </nav>
     </div>
   )
 }
