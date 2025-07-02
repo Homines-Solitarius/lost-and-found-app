18a984746da684aca95ecbd439fca62d703f16cc
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-02 22:44:25 +0100

    Designed the footer of the app

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+      <div className="footer">
+          <b>
+          <h3>All Rights Reserved. | 2025</h3>
+          </b>
+    </div>
+  )
+}
+
+export default Footer
