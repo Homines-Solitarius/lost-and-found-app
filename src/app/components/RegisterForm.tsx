10b75ef2ff43824e7e76b3306bd25d4612e22218
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-03 03:37:44 +0100

    Made button text bolder

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 import { FaUserCircle } from 'react-icons/fa';
 
-const LoginForm = () => {
+const RegisterForm = () => {
     const [matricNumber, setMatricNumber] = useState('');
     const [password, setPassword] = useState('')
 
@@ -24,17 +24,19 @@ const LoginForm = () => {
     }
   return (
     <div className="p-5 m-auto">
-        <h3 className='text-xl font-bold text-center m-4'>Welcome back you&apos;ve been missed.</h3>
+        <h3 className='text-xl font-bold text-center m-4'>Let&apos;s create an account for you.</h3>
           <form onSubmit={handleForm} className='relative bg-gray-100 p-4 rounded max-w-80'>
               <FaUserCircle size={60} className='mx-auto my-4'/>
             <label htmlFor="Title">Matric Number</label>
             <input type="text" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userMatric" id="userMatric" value={matricNumber} onChange={(e) => setMatricNumber(e.target.value)}/>
             <label htmlFor="Title">Password</label>
               <input type="password" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userPassword" id="userPassword" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <button type="submit" className='bg-blue-700 p-2 my-3 text-white font-bold w-full rounded-md'>Login</button>
+            <label htmlFor="Title">Confirm Password</label>
+              <input type="password" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userPassword" id="userPassword" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button type="submit" className='bg-blue-700 p-2 my-3 text-white font-bold w-full rounded-md'>Register</button>
         </form>
    </div>
   )
 }
 
-export default LoginForm
+export default RegisterForm
