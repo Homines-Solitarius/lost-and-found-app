a50a3a8d21ca43064850385c1b30a21e88c5f475
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-03 15:01:37 +0100

    Feat: Worked on responsiveness and text-sizes

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import React, { useState } from 'react'
 import { FaUserCircle } from 'react-icons/fa';
 
@@ -24,15 +25,19 @@ const LoginForm = () => {
     }
   return (
     <div className="p-5 m-auto">
-        <h3 className='text-xl font-bold text-center m-4'>Welcome back you&apos;ve been missed.</h3>
-          <form onSubmit={handleForm} className='relative bg-gray-100 p-4 rounded max-w-80'>
+        <h3 className='text-xl font-bold text-center m-4 lg:text-3xl lg:my-10'>Welcome back you&apos;ve been missed.</h3>
+          <form onSubmit={handleForm} className='relative block bg-gray-100 p-4 rounded lg:w-100 lg:m-auto'>
               <FaUserCircle size={60} className='mx-auto my-4'/>
             <label htmlFor="Title">Matric Number</label>
             <input type="text" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userMatric" id="userMatric" value={matricNumber} onChange={(e) => setMatricNumber(e.target.value)}/>
             <label htmlFor="Title">Password</label>
               <input type="password" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userPassword" id="userPassword" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <button type="submit" className='bg-blue-700 p-2 my-3 text-white font-bold w-full rounded-md'>Login</button>
-        </form>
+        <button type="submit" className='bg-blue-700 p-2 my-3 text-white font-bold w-full rounded-md'>Login</button>
+        <div className='flex items-center gap-1'>
+          <p className='text-sm'>Don&apos;t have an account?</p>
+          <Link href='/register' className='text-blue-700'>Register now</Link>
+        </div>
+      </form>
    </div>
   )
 }
