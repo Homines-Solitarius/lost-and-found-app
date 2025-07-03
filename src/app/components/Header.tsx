c77e245f89a2af304d9954bb61bf0756f6c06f5b
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-03 01:53:13 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,20 +3,24 @@ import "../globals.css"
 import Link from "next/link"
 
 const Header = () => {
+  const navigation = [
+    { name: 'Home', href: '/' },
+    { name: 'Report Lost Item', href: '/report_item' },
+    { name: 'Contact Us', href: '#' },
+  ]
   return (
-      <div className="flex p-8 items-center justify-between bg-sky-500">
-        <div className="flex flex-row items-center gap-2">
-          <FaLock color="white" />
-          <span className="text-xl font-bold text-white">Fpe Recover</span>
-        </div>
-        <nav className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-6 p-4 bg-white shadow-md">
-          <Link href="/" className="text-gray-700 hover:text-blue-600 font-medium text-base">
-            Home
+    <div className="flex px-20 items-center justify-between bg-blue-700">
+      <div className="flex flex-row items-center gap-2 ">
+        <FaLock color="white" />
+        <span className="text-xl font-bold text-white">Fpe Recover</span>
+      </div>
+      <nav className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-6 p-4">
+        {navigation.map((item) => (
+          <Link key={item.name} href={item.href} className="text-white rounded-lg hover:bg-white hover:text-blue-700 font-bold text-md p-2 transition-all duration-500 ease-out">
+            {item.name}
           </Link>
-          <Link href="/about" className="text-gray-700 hover:text-blue-600 font-medium text-base">
-            About
-          </Link>
-        </nav>
+        ))}
+      </nav>
     </div>
   )
 }
