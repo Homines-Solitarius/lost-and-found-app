709f4bf8f7d179c885d644d5b4f20a348ad9a07f
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-03 02:51:15 +0100

    Refactor Home component for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,22 @@ import HeroImage from '/public/hand.jpg'
 import Button from './components/Button'
 
 const Home = () => {
-  const handleClick = () => {
-    alert("Get Started Clicke")
-  }
+
   return (
     <>
      <div className='pt-15'>
-        <h2 className='sm:text-2xl text-2xl font-bold text-center'>Report <span className='text-red-500'>Lost</span> and <span className='text-green-500'>Found</span> Items.</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi veniam aperiam illo error, tenetur esse unde animi iusto quaerat dolores asperiores totam, commodi earum. Mollitia doloremque minima maxime tempore neque.</p>
-        <div className='hero-btns'>
-          <Button text="Get Started" onClick={handleClick} />
-          <Link href='/login'>
-            <Button color='#222' text="Login"/>
+        <h2 className='text-3xl font-bold text-center mb-6'>Report <span className='text-red-500'>Lost</span> and <span className='text-green-500'>Found</span> Items.</h2>
+      <p className='w-75 m-auto px-5 text-center text-gray-800 text-sm mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi veniam aperiam illo error, tenetur esse unde animi iusto quaerat dolores asperiores totam, commodi earum. Mollitia doloremque minima maxime tempore neque.</p>
+        <div className='flex items-center gap-4 m-auto my-3 justify-center'>
+          <Link href='/login' className='bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>
+            <Button text="Login"/>
           </Link>
-          
+          <Link href='/login' className='bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded'>
+            <Button text="Register"/>
+          </Link>
+        </div>
       </div>
-      </div>
-      <Image alt='hero-image' src={HeroImage} className='w-40'></Image>
+      <Image alt='hero-image' src={HeroImage} className='w-70 m-auto'></Image>
     </>
   )
 }
