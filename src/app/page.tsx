e1bb4019cef51fcc1724b2e7501009e29853a422
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-03 15:00:12 +0100

    Feat: Worked on responsiveness across multiple devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,19 @@ const Home = () => {
 
   return (
     <>
-     <div className='pt-15'>
-        <h2 style={{lineHeight: 1.7}} className='text-3xl font-bold text-center px-3 mb-6'>Recover <span className='text-red-500 px-2 '>Lost</span>Items With <span className='text-blue-500 p-2 rounded-full border-2 border-dashed'>Ease</span>.</h2>
-      <p className='w-75 m-auto px-5 text-center text-gray-800 text-sm mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi veniam aperiam illo error, tenetur esse unde animi iusto quaerat dolores asperiores totam, commodi earum. Mollitia doloremque minima maxime tempore neque.</p>
-        <div className='flex gap-4 m-auto my-3 justify-center'>
-          <Link href='/login' className='bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg'>
+     <div className='pt-10 lg:mx-10'>
+        <h2 style={{ lineHeight: 1.7 }} className='text-3xl w-full m-auto lg:w-full font-bold text-center px-3 mb-6 lg:text-6xl lg:mt-10'>Recover <span className='text-red-500 px-3'>Lost</span>Items With <span className='text-blue-500 p-2 px-4 rounded-full border-5 border-dashed'>Ease</span>.</h2>
+        <p className='text-center px-2 lg:w-230 lg:m-auto lg:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi veniam aperiam illo error, tenetur esse unde animi iusto quaerat dolores asperiores totam, commodi earum. Mollitia doloremque minima maxime tempore neque.</p>
+        <div className='flex gap-4 m-auto my-5 justify-center lg:gap-6 lg:mt-10'>
+          <Link href='/login' className='bg-blue-700 hover:bg-blue-500 text-center text-white font-bold py-2 px-4 rounded-lg sm:w-50 lg:text-xl lg:w-40'>
             <Button text="Login"/>
           </Link>
-          <Link href='/register' className='bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-lg'>
+          <Link href='/register' className='bg-black hover:bg-gray-800 text-center text-white font-bold py-2 px-4 rounded-lg lg:text-xl lg:w-40'>
             <Button text="Register"/>
           </Link>
         </div>
       </div>
-      <Image alt='hero-image' src={HeroImage} className='w-70 m-auto rounded'></Image>
+      <Image alt='hero-image' src={HeroImage} className='w-70 m-auto rounded lg:w-140 lg:w-130 '></Image>
     </>
   )
 }
