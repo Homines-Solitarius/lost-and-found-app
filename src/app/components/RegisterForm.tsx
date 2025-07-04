1b1bce6eceb970d87add1d2ef278983e3e17484d
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-04 16:44:06 +0100

    Created Student Login, remove page title.

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -21,9 +21,8 @@ const RegisterForm = () => {
     }
   return (
     <div className="p-5 m-auto">
-        <h3 className='text-xl font-bold text-center m-4'>Let&apos;s create an account for you.</h3>
           <form onSubmit={handleForm} className='relative bg-gray-100 p-4 rounded max-w-80'>
-              <FaUserCircle size={60} className='mx-auto my-4'/>
+        <FaUserCircle size={60} className='mx-auto my-4 bg-blue-700 text-white rounded-full'/>
             <label htmlFor="Title">Matric Number</label>
             <input type="text" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userMatric" id="userMatric" value={matricNumber} onChange={(e) => setMatricNumber(e.target.value)}/>
             <label htmlFor="Title">Password</label>
