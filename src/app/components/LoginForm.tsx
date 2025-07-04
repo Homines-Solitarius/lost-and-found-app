6a3a2a84016aac4c8ce7edafb93f835be243138c
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-04 16:22:10 +0100

    Remove role-switch button for better UX.

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -22,22 +22,25 @@ const LoginForm = () => {
             alert('Invalid Credentials...')
             return;
         }
-    }
+  }
+  
   return (
-    <div className="p-5 m-auto">
-        <h3 className='text-xl font-bold text-center m-4 lg:text-3xl lg:my-10'>Welcome back you&apos;ve been missed.</h3>
-          <form onSubmit={handleForm} className='relative block bg-gray-100 p-4 rounded lg:w-100 lg:m-auto'>
-              <FaUserCircle size={60} className='mx-auto my-4'/>
-            <label htmlFor="Title">Matric Number</label>
-            <input type="text" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userMatric" id="userMatric" value={matricNumber} onChange={(e) => setMatricNumber(e.target.value)}/>
-            <label htmlFor="Title">Password</label>
-              <input type="password" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userPassword" id="userPassword" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <button type="submit" className='bg-blue-700 p-2 my-3 text-white font-bold w-full rounded-md'>Login</button>
-        <div className='flex items-center gap-1'>
-          <p className='text-sm'>Don&apos;t have an account?</p>
-          <Link href='/register' className='text-blue-700'>Register now</Link>
-        </div>
-      </form>
+    <div className="p-5 m-auto flex-1">
+      <div className="m-auto">
+        <form onSubmit={handleForm} className='relative block bg-gray-100 p-4 rounded lg:w-100 lg:m-auto'>
+          <FaUserCircle size={60} className='mx-auto my-4' />
+          <label htmlFor="Title">Matric Number</label>
+          <input type="text" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userMatric" id="userMatric" value={matricNumber} onChange={(e) => setMatricNumber(e.target.value)} />
+          <label htmlFor="Title">Password</label>
+          <input type="password" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userPassword" id="userPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit" className='bg-blue-700 p-2 my-3 text-white font-bold w-full rounded-md'>Login</button>
+          <div className='flex items-center gap-1'>
+            <p className='text-sm mr-1'>Don&apos;t have an account?</p>
+            <Link href='/register' className='text-blue-700 mr-auto'>Register now</Link>
+          </div>
+        </form>
+        { /* Administrator login */}
+         </div>
    </div>
   )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,24 @@
+'use client'
+
+import { useState } from "react"
+import AdminLogin from "../components/AdminLogin"
 import LoginForm from "../components/LoginForm"
 
 const Login = () => {
+
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const SwitchForm = () => {
+    setIsAdmin(!isAdmin)
+  }
+
   return (
-      <>
-      <LoginForm/>
+    <>
+      <h3 className='text-xl font-bold text-center m-4 lg:text-3xl lg:my-10'> {isAdmin ? "Administrator Login" : "Student Login"}</h3>
+      <div className="flex justify-center w-full m-auto overflow-x-hidden transition-all duration-300 ease-in-out">
+        {isAdmin ? <AdminLogin /> : <LoginForm/> }
+      </div>
+      <button className="block ml-auto mr-5 mb-3 bg-gray-900 text-white font-bold p-2 rounded-md" onClick={SwitchForm}>{isAdmin ? "Login as Student" : "Login as admin"}</button>
       </>
   )
 }
