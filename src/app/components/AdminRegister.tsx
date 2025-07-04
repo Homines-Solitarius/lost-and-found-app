928fd3d27001ca2903a3abf574c86f1071dbd82c
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-04 16:43:37 +0100

    Created admin registration

---
+++ src/app/components/AdminRegister.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Link from 'next/link';
+import React, { useState } from 'react'
+import { FaUserCircle } from 'react-icons/fa';
+
+const AdminRegister = () => {
+    const [matricNumber, setMatricNumber] = useState('');
+    const [password, setPassword] = useState('')
+
+
+    const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // Prevents
+
+        const storedMatric = 'CS20230200712';
+        const storedPassword = '123456'
+
+        //
+        if (matricNumber == storedMatric && password == storedPassword) {
+            console.log("Login Successful");
+        } else {
+            alert('Invalid Credentials...')
+            return;
+        }
+    }
+
+    return (
+        <div className="p-5 m-auto">
+            <div className="">
+                <form onSubmit={handleForm} className='relative block bg-gray-100 p-4 rounded lg:w-100 lg:m-auto'>
+                    <FaUserCircle size={60} className='mx-auto my-4 bg-blue-700 text-white rounded-full' />
+                    <label htmlFor="Title">Staff Id</label>
+                    <input type="text" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userMatric" id="userMatric" value={matricNumber} onChange={(e) => setMatricNumber(e.target.value)} />
+                    <label htmlFor="Title">Password</label>
+                    <input type="password" className='border-2 border-gray-200 my-1 p-1 w-full rounded-md' name="userPassword" id="userPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit" className='bg-blue-700 p-2 my-3 text-white font-bold w-full rounded-md'>Register</button>
+                    <div className='flex items-center gap-1'>
+                        <p className='text-sm mr-1'>Don&apos;t have an account?</p>
+                        <Link href='/register' className='text-blue-700 mr-auto'>Register now</Link>
+                    </div>
+                </form>
+                { /* Administrator login */}
+            </div>
+        </div>
+    )
+}
+
+export default AdminRegister
