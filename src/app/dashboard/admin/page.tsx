19c4923f1d1d0be17dcb36243f333c8e755a28d3
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-04 20:18:46 +0100

    Abeg work oo

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -3,16 +3,17 @@
 import Button from '@/app/components/Button'
 import React, { useState } from 'react'
 import { FaDoorOpen } from 'react-icons/fa'
+import Home from './Home'
 
 const AdminDashboard = () => {
 
     const [isActive, setIsActive] = useState(false);
     const [isIndex, setIsIndex] = useState(0);
 
-    const Pages = [
-        { id: 1, name: 'Home', content: "Hello 1" },
-        { id: 2, name: 'Reports', content: "Hello 2" },
-        { id: 3, name: 'Settings', content: "Hello 3" },
+    const pages = [
+        { id: 1, name: 'Home', content: <Home/> },
+        { id: 2, name: 'Reports', content: <Home /> },
+        { id: 3, name: 'Settings', content: <Home /> },
     ]
 
     const handleDisplay = (id: number) => {
@@ -23,7 +24,7 @@ const AdminDashboard = () => {
         <div className="flex flex-row w-full h-full">
             <div className="inline-block bg-blue-600 text-white w-60 h-140 overflow-y-hidden py-3 px-2 shadow-md">
                 <h2 className='font-bold pl-2 my-3 text-xl'>My DashBoard</h2>
-                {Pages.map((item) => (
+                {pages.map((item) => (
                     <li onClick={() => {
                         setIsActive(!isActive);
                         handleDisplay(item.id);
@@ -38,7 +39,9 @@ const AdminDashboard = () => {
             </div>
             {/* Display */}
             <div className="bg-gray-200 w-full h-140 p-4 overflow-y-scroll">
-                {Pages[isIndex -1].content}
+                <div className='m-3 ml-6'>
+                    {isIndex && pages[isIndex-1]['content']}
+                </div>
             </div>
         </div>
     )
