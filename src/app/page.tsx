a65f507fd9600f9f55c911220dcf10ccdfec877b
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-05 23:13:15 +0100

    Removed extra padding on element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const Home = () => {
         <h2 style={{ lineHeight: 1.7 }} className='text-3xl w-full m-auto lg:w-full font-bold text-center px-3 mb-6 lg:text-6xl lg:mt-10'>Recover <span className='text-red-500 px-3'>Lost</span>Items With <span className='text-blue-500 p-2 px-4 rounded-full border-5 border-dashed'>Ease</span>.</h2>
         <p className='text-center px-2 lg:w-230 lg:m-auto lg:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi veniam aperiam illo error, tenetur esse unde animi iusto quaerat dolores asperiores totam, commodi earum. Mollitia doloremque minima maxime tempore neque.</p>
         <div className='flex gap-4 m-auto my-5 justify-center lg:gap-6 lg:mt-10'>
-          <Link href='/login' className='bg-blue-700 hover:bg-blue-500 text-center text-white font-bold py-2 px-4 rounded-lg sm:w-50 lg:text-xl lg:w-40'>
+          <Link href='/login' className='bg-blue-700 hover:bg-blue-500 text-center text-white font-bold py-2 px-2 rounded-lg sm:w-50 lg:text-xl lg:w-40'>
             <Button text="Login"/>
           </Link>
           <Link href='/register' className='bg-black hover:bg-gray-800 text-center text-white font-bold py-2 px-4 rounded-lg lg:text-xl lg:w-40'>
